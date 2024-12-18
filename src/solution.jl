85cd40ae8e9d66a181c344c38ef569fc0ea143c4
Iago Leal <iago@iagoleal.com>
2024-12-18 16:38:33 -0500

    Method `in` to check whether a bitstring is sampleable from a wavefunction

--- src/solution.jl
+++ src/solution.jl
@@ -14,6 +14,12 @@ Sample a bitstring from a (quantum) probability distribution.
 sample(psi::Distribution) = ITensorMPS.sample!(psi.tensor) .- 1
 
 
+"""
+  in(xs, psi::Distribution [; cutoff)
+
+Whether the vector `xs` has a positive probability of being sampleable from `psi`.
+When setting `cutoff`, it will be used as the minimum probability considered positive.
+"""
 function Base.in(bs, psi::Distribution; cutoff = 1e-8)
   tn = psi.tensor
   sites = siteinds(tn)
