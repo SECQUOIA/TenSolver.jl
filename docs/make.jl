59342468d463b490df36f9c248306aac26a2331d
Iago Leal <iago@iagoleal.com>
2024-12-20 18:32:13 -0500

    Deploy docs to github

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,20 @@ using Documenter
 using TenSolver
 
 makedocs(
-    sitename = "TenSolver",
-    format = Documenter.HTML(),
+    sitename = "TenSolver.jl",
+    format = Documenter.HTML(
+      mathengine = Documenter.KaTeX(),
+    ),
     modules = [TenSolver]
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+# Deploy site to Github Pages
+if !("local" in ARGS)
+    deploydocs(
+        repo = "github.com/SECQUOIA/TenSolver.jl.git"
+    )
+end
+
