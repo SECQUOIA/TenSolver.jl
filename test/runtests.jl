0e199285dec209a22507fb3c23bfe6658cdfe2a6
Iago Leal de Freitas <iago@iagoleal.com>
2026-06-24 15:51:58 -0300

    Organize runtests file

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,34 +11,34 @@ filepath(x) = joinpath(dirname(@__FILE__), x)
 # Exact solvers. No testset on this file
 include(filepath("utils.jl"))
 
+
+#----------------------------------------------------------#
+#                         Test sets                        #
+#----------------------------------------------------------#
+
+# Ising <-> QUBO utilities
 include(filepath("ising_conversion.jl"))
-
-@testset "Ill-formed input" begin
-  dim = 4
-
-  # Should throw when the matrix is not square
-  Q = randn(dim, dim - 2)
-  @test_throws DimensionMismatch minimize(Q)
-
-  Q = randn(dim - 2, dim)
-  @test_throws DimensionMismatch minimize(Q)
-end
 
 # Traditional interface
 include(filepath("qubo.jl"))
 include(filepath("pubo.jl"))
-# Iteration Log utilities
-include(filepath("log.jl"))
+
 # JuMP interface
 include(filepath("jump.jl"))
-# QUBODrivers.jl and Aqua.jl test suites
-include(filepath("external.jl"))
+
+# Iteration Log utilities
+include(filepath("log.jl"))
 
 # Cases from papers
-include(filepath("cases/vrp.jl"))
+@testset "Real Models" begin
+  include(filepath("cases/vrp.jl"))
+end
 
 # HDF5 snapshot callback
 include(filepath("hdf5.jl"))
+
+# QUBODrivers.jl and Aqua.jl test suites
+include(filepath("external.jl"))
 
 # Documentation tests
 include(filepath("doctests.jl"))
